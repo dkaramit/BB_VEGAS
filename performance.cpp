11cf5893ee2c448ee533c29a016fa9bff640d86d
dkarmit <dkarmit@yahoo.com>
2020-06-26 02:47:44 +0200

    timing more things

--- performance.cpp
+++ performance.cpp
@@ -14,39 +14,102 @@ void f(LD x[NDim], LD *retrn){
 
 using Time = std::chrono::high_resolution_clock;
 using duration = std::chrono::duration<double> ;
-int main(){
-    LD res,sigma,R;
-    
+
+
+
+void timeSubDiv( ){
+    cout<<"========================Timing SubDivision========================"<<endl;
+
     auto start = Time::now();
     auto finish = Time::now();
     auto t0 = Time::now();
     auto t1 = Time::now();
     duration  dur;
 
-
-    // declaration of integral should be slow
-    t0 = Time::now();
-    VEGAS<LD,Func,NDim,NBin,NBinInit>  Integral(f,NPoints , NBatches , NAdapts, AdaptPoints, 
+    VEGAS<LD,Func,NDim,NBin,NBinInit>  Int(f,NPoints , NBatches , NAdapts, AdaptPoints, 
                                                 NAdaptSubDivs, SubDivPoints, constK , alpha);
-    t1 = Time::now();
-    dur = t1-t0;
-    cout<< dur.count()  <<endl;
-
 
 
     t0 = Time::now();
 
-    for (int i = 0; i < 50; i++){
+    for (int i = 0; i < 10; i++){
         start = Time::now();
-        R=Integral.Integrate( &res,&sigma );
-        cout<<"Result= "<<  res <<" +/- "<<  sigma/res*100 <<"%" <<", with R="<< R<<"\t\t";
+        Int.SubDivision();      
         finish = Time::now();
         dur = (finish - start);
-        cout<< dur.count()  <<endl;
+        cout<<"run: "<<i <<"  dt="<<dur.count()<<" s"  <<endl;
     }
     t1 = Time::now();
     dur = t1-t0;
     cout<< dur.count()  <<endl;
+
+
+}
+
+
+void timeUpBin( ){
+    cout<<"========================Timing UpdateBins========================"<<endl;
+
+    auto start = Time::now();
+    auto finish = Time::now();
+    auto t0 = Time::now();
+    auto t1 = Time::now();
+    duration  dur;
+
+    VEGAS<LD,Func,NDim,NBin,NBin>  Int(f,NPoints , NBatches , NAdapts, AdaptPoints, 
+                                                NAdaptSubDivs, SubDivPoints, constK , alpha);
+
+
+    t0 = Time::now();
+
+    for (int i = 0; i < 10; i++){
+        start = Time::now();
+        for(int _=0 ; _<500;++_){Int.UpdateBins(); }     
+        finish = Time::now();
+        dur = (finish - start);
+        cout<<"run: "<<i <<"  dt="<<dur.count()<<" s"  <<endl;
+    }
+    t1 = Time::now();
+    dur = t1-t0;
+    cout<< dur.count()  <<endl;
+
+
+}
+
+void timeIntTot( ){
+    cout<<"========================Timing IntegrateTot========================"<<endl;
+    LD res,var;
+    auto start = Time::now();
+    auto finish = Time::now();
+    auto t0 = Time::now();
+    auto t1 = Time::now();
+    duration  dur;
+
+    VEGAS<LD,Func,NDim,NBin,NBin>  Int(f,NPoints , NBatches , NAdapts, AdaptPoints, 
+                                                NAdaptSubDivs, SubDivPoints, constK , alpha);
+
+
+    t0 = Time::now();
+
+    for (int i = 0; i < 10; i++){
+        start = Time::now();
+        for(int _=0 ; _<70;++_){Int.IntegrateTot(&res,&var);}     
+        finish = Time::now();
+        dur = (finish - start);
+        cout<<"run: "<<i <<"  dt="<<dur.count()<<" s"  <<endl;
+    }
+    t1 = Time::now();
+    dur = t1-t0;
+    cout<< dur.count()  <<endl;
+}
+
+
+int main(){
+    
+    timeSubDiv();
+    timeUpBin();
+    timeIntTot();
+
 
 
     return 0;
