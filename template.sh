d8c2cd050871d2276e8deee2b78e20740747edaa
dimitri <dkaramit@yahoo.com>
2021-07-25 22:42:10 +0200

    removed auxiliary files

--- template.sh
+++
@@ -1,8 +0,0 @@
-
-
-for file in $(find . -regextype egrep -regex '.*\.hpp'); do
-    perl -pe 's/VEGAS\_Template/template<class LD, int NDim, int NBin, int NBinInit, class RandEn>/g; 
-    s/VEGAS\_Namespace/VEGAS<LD,NDim,NBin,NBinInit,RandEn>/g' $file >tmp 
-
-    less tmp > $file       
-done
